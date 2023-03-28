c7e35a47bc6d9ae31c90efddafc2e99c40a3e795
Michele Giornetta <michelegiornetta@gmail.com>
2023-03-28 13:39:24 +0200

    kafka listener now only handles one handler per topic

--- kafka/listener.go
+++ kafka/listener.go
@@ -9,43 +9,40 @@ import (
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
-type listener struct {
+type Listener struct {
 	client *kgo.Client
 
 	wg sync.WaitGroup
 
 	lock     sync.RWMutex
-	handlers map[events.Topic][]events.Handler
+	handlers map[events.Topic]events.Handler
 }
 
-func NewListener(client *kgo.Client) (*listener, error) {
-	l := &listener{
+func NewListener(client *kgo.Client) (*Listener, error) {
+	l := &Listener{
 		client:   client,
-		handlers: make(map[events.Topic][]events.Handler),
+		handlers: make(map[events.Topic]events.Handler),
 	}
 
 	return l, nil
 }
 
-func (l *listener) Handle(topic events.Topic, handler events.Handler) {
+func (l *Listener) Handle(topic events.Topic, handler events.Handler) {
 	l.lock.Lock()
 	defer l.lock.Unlock()
 
-	handlers, ok := l.handlers[topic]
+	_, ok := l.handlers[topic]
 	if !ok {
-		handlers = make([]events.Handler, 0, 1)
 		l.client.AddConsumeTopics(topic.String())
 	}
 
-	// Should check if handler is already in the list but w/e
-	l.handlers[topic] = append(handlers, handler)
+	l.handlers[topic] = handler
 }
 
-func (l *listener) Listen(ctx context.Context) error {
+func (l *Listener) Listen(ctx context.Context) error {
 	for {
 		fetches := l.client.PollFetches(ctx)
 		if errs := fetches.Errors(); errs != nil {
-			// TODO Improve error handling
 			l.wg.Wait()
 			if errs[0].Err == context.Canceled {
 				return nil
@@ -68,19 +65,23 @@ func (l *listener) Listen(ctx context.Context) error {
 
 }
 
-func (l *listener) handleEvent(e events.Event) {
+func (l *Listener) handleEvent(e events.Event) {
 	l.lock.RLock()
 	defer l.lock.RUnlock()
 
-	for _, h := range l.handlers[e.Topic()] {
-		l.wg.Add(1)
-		go func(h events.Handler) {
-			if err := h.Handle(e); err != nil {
-				// TODO Do some error Handling
-				log.Println(err)
-			}
-			l.wg.Done()
-		}(h)
+	handler, ok := l.handlers[e.Topic()]
+	if !ok {
+		return
 	}
+
+	l.wg.Add(1)
+	go func(h events.Handler) {
+		if err := h.Handle(e); err != nil {
+			// TODO Do some error Handling
+			log.Println(err)
+		}
+		l.wg.Done()
+	}(handler)
+
 	l.wg.Done()
 }
